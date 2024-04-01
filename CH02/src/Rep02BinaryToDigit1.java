f01b68d65e7d80f34563ae5f8f245995eaf8c602
kinnwonjin <jin@McBook-Pro.local>
2024-04-01 10:27:58 +0900

    sync

---
+++ CH02/src/Rep02BinaryToDigit1.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+
+public class Rep02BinaryToDigit1 {
+	public static void main(String[] args) {
+		String input;
+		input = JOptionPane.showInputDialog("Enter a 4-bit binary number: ");
+		JOptionPane.showMessageDialog(null, "The decimal value is: " + input);
+		
+		System.exit(0);
+		
+	}
+
+}
